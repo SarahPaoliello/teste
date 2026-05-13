bf66b6c283cf3144d80ce0e680a2b97e92bd9404
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-13 19:10:47 -0300

    Update voto_service.py

--- services/voto_service.py
+++ services/voto_service.py
@@ -1,1 +1,37 @@
+from database.conexao import supabase
 
+
+def registrar_voto(usuario_id, apresentacao_id, nota):
+
+    verificar = (
+        supabase
+        .table("votos")
+        .select("id")
+        .eq("usuario_id", usuario_id)
+        .eq("apresentacao_id", apresentacao_id)
+        .execute()
+    )
+
+    if verificar.data:
+        return "Você já votou"
+
+    return (
+        supabase
+        .table("votos")
+        .insert({
+            "usuario_id": usuario_id,
+            "apresentacao_id": apresentacao_id,
+            "nota": nota
+        })
+        .execute()
+    )
+
+
+def listar_votos():
+
+    return (
+        supabase
+        .table("votos")
+        .select("*")
+        .execute()
+    )
