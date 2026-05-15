0dd818fa58f372141c52c89dd8a4b6e915d7b3e3
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-15 12:05:14 -0300

    Update criar_desafio.py

--- pages/telas/criar_desafio.py
+++ pages/telas/criar_desafio.py
@@ -1,31 +1,14 @@
-from database.conexao import supabase
 import streamlit as st
 
 from services.desafio_service import criar_desafio
 
-def listar_desafios():
 
-    resposta = (
-        supabase
-        .table("desafios")
-        .select("*")
-        .order("id")
-        .execute()
-    )
 def tela_criar_desafio():
 
-    return resposta.data
     st.title("Criar desafio")
 
     with st.form("form_desafio"):
 
-def criar_desafio(
-    titulo,
-    descricao,
-    max_participantes,
-    data_fechamento,
-    criado_por
-):
         titulo = st.text_input(
             "Título"
         )
@@ -40,25 +23,18 @@ def criar_desafio(
             step=1
         )
 
-    dados = {
         data_fechamento = st.date_input(
             "Data de fechamento"
         )
 
-        "titulo": titulo,
         criar = st.form_submit_button(
             "Criar desafio"
         )
 
-        "descricao": descricao,
     if criar:
 
-        "max_participantes": int(
-            max_participantes
-        ),
         usuario = st.session_state.usuario_logado
 
-        "data_fechamento": data_fechamento.isoformat(),
         criar_desafio(
             titulo,
             descricao,
@@ -67,19 +43,10 @@ def criar_desafio(
             usuario["id"]
         )
 
-        "criado_por": int(
-            criado_por
         st.success(
             "Desafio criado com sucesso"
         )
-    }
 
-    return (
-        supabase
-        .table("desafios")
-        .insert(dados)
-        .execute()
-    )
         st.session_state.pagina = "desafios"
 
         st.rerun()
@@ -88,14 +55,6 @@ def criar_desafio(
 
     if st.button("Voltar"):
 
-def deletar_desafio(id_desafio):
         st.session_state.pagina = "desafios"
 
-    return (
-        supabase
-        .table("desafios")
-        .delete()
-        .eq("id", id_desafio)
-        .execute()
-    )
         st.rerun()
