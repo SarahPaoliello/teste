8015bd0ed50c7e008633729ad35ee0593e55aa68
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-15 10:16:33 -0300

    Update auth_service.py

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,6 +1,75 @@
-def usuario_e_admin(usuario):
+import hashlib
+import re
 
-    if not usuario:
-        return False
+from database.conexao import supabase
 
-    return usuario["tipo_usuario"] == "admin"
+
+def criptografar_senha(senha):
+
+    return hashlib.sha256(
+        senha.encode()
+    ).hexdigest()
+
+
+def senha_valida(senha):
+
+    if len(senha) < 8:
+        return "A senha deve ter no mínimo 8 caracteres"
+
+    if not re.search(r"[A-Z]", senha):
+        return "A senha deve conter letra maiúscula"
+
+    if not re.search(r"\d", senha):
+        return "A senha deve conter número"
+
+    return "ok"
+
+
+def login_usuario(email, senha):
+
+    resposta = (
+        supabase
+        .table("usuarios")
+        .select("*")
+        .eq("email", email)
+        .eq("senha", criptografar_senha(senha))
+        .execute()
+    )
+
+    if resposta.data:
+        return resposta.data[0]
+
+    return None
+
+
+def cadastrar_usuario(
+    nome,
+    email,
+    tipo_usuario,
+    senha
+):
+
+    validar = senha_valida(senha)
+
+    if validar != "ok":
+        return validar
+
+    verificar = (
+        supabase
+        .table("usuarios")
+        .select("id")
+        .eq("email", email)
+        .execute()
+    )
+
+    if verificar.data:
+        return "E-mail já cadastrado"
+
+    supabase.table("usuarios").insert({
+        "nome": nome,
+        "email": email,
+        "tipo_usuario": tipo_usuario,
+        "senha": criptografar_senha(senha)
+    }).execute()
+
+    return "ok"
