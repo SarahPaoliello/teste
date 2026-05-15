17d14cd45d0a1acc5527854cbe7611c237f38fa1
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-15 10:23:47 -0300

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -1,1 +1,40 @@
+import streamlit as st
 
+
+def tela_home():
+
+    usuario = st.session_state.usuario_logado
+
+    st.title(
+        f"Bem-vindo(a), {usuario['nome']}"
+    )
+
+    st.divider()
+
+    st.subheader(
+        "Desafios disponíveis"
+    )
+
+    st.info(
+        "Nenhum desafio cadastrado"
+    )
+
+    st.divider()
+
+    st.subheader(
+        "Desafios disponíveis para votação"
+    )
+
+    st.info(
+        "Nenhuma apresentação disponível"
+    )
+
+    st.divider()
+
+    st.subheader(
+        "Mini-provas"
+    )
+
+    st.warning(
+        "Sistema em construção"
+    )
