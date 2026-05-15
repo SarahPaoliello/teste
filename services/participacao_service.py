7332a313278d16f4e8de7fa97219825946e601b4
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-15 12:15:37 -0300

    Create participacao_service.py

---
+++ services/participacao_service.py
@@ -0,0 +1,50 @@
+from database.conexao import supabase
+
+
+def participar_desafio(
+    desafio_id,
+    usuario_id
+):
+
+    verificar = (
+        supabase
+        .table("participantes_desafio")
+        .select("*")
+        .eq("desafio_id", desafio_id)
+        .eq("usuario_id", usuario_id)
+        .execute()
+    )
+
+    if verificar.data:
+
+        return False
+
+    supabase.table(
+        "participantes_desafio"
+    ).insert({
+
+        "desafio_id": desafio_id,
+
+        "usuario_id": usuario_id
+
+    }).execute()
+
+    return True
+
+
+def listar_participantes(
+    desafio_id
+):
+
+    resposta = (
+        supabase
+        .table("participantes_desafio")
+        .select("""
+            *,
+            usuarios(nome)
+        """)
+        .eq("desafio_id", desafio_id)
+        .execute()
+    )
+
+    return resposta.data
