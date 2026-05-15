f81f4ecd562e1753be4028925ffa97458e1ce96a
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-15 11:39:14 -0300

    Update navbar.py

--- components/navbar.py
+++ components/navbar.py
@@ -15,39 +15,62 @@ def mostrar_menu():
 
         st.divider()
 
-        if st.button("Home"):
+        if st.button(
+            "Home",
+            key="menu_home"
+        ):
+
             st.session_state.pagina = "home"
+
             st.rerun()
 
-        if st.button("Desafios"):
+        if st.button(
+            "Desafios",
+            key="menu_desafios"
+        ):
+
             st.session_state.pagina = "desafios"
+
             st.rerun()
 
-        if st.button("Votação"):
+        if st.button(
+            "Votação",
+            key="menu_votacao"
+        ):
+
             st.session_state.pagina = "votacao"
+
             st.rerun()
 
-        if st.button("Mini-provas"):
+        if st.button(
+            "Mini-provas",
+            key="menu_miniprovas"
+        ):
+
             st.session_state.pagina = "mini_provas"
+
             st.rerun()
 
         if usuario["tipo_usuario"] == "admin":
 
-            if st.button("Admin"):
+            if st.button(
+                "Admin",
+                key="menu_admin"
+            ):
+
                 st.session_state.pagina = "admin"
+
                 st.rerun()
 
         st.divider()
 
-        if st.button("Sair"):
+        if st.button(
+            "Sair",
+            key="menu_sair"
+        ):
 
             st.session_state.usuario_logado = None
+
             st.session_state.pagina = "login"
-
-            st.rerun()
-
-        if st.button("Desafios"):
-
-            st.session_state.pagina = "desafios"
 
             st.rerun()
