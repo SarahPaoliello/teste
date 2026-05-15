be683a4599ef146b713095f8d3e0662d1e1983da
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-15 12:02:46 -0300

    Update desafio_service.py

--- services/desafio_service.py
+++ services/desafio_service.py
@@ -19,23 +19,44 @@ def criar_desafio(
     descricao,
     max_participantes,
     data_fechamento,
-    criado_por
+    criador_id
 ):
 
-    supabase.table("desafios").insert({
+    dados = {
 
         "titulo": titulo,
-        "descricao": descricao,
-        "max_participantes": max_participantes,
-        "data_fechamento": str(data_fechamento),
-        "criado_por": criado_por
 
-    }).execute()
+        "descricao": descricao,
+
+        "max_participantes": int(
+            max_participantes
+        ),
+
+        "data_fechamento": data_fechamento.isoformat(),
+
+        # ALTERADO PARA O NOME CORRETO DA TABELA
+        "criador_id": int(
+            criador_id
+        ),
+
+        # STATUS PADRÃO
+        "status": "aberto"
+    }
+
+    return (
+        supabase
+        .table("desafios")
+        .insert(dados)
+        .execute()
+    )
 
 
 def deletar_desafio(id_desafio):
 
-    supabase.table("desafios")\
-        .delete()\
-        .eq("id", id_desafio)\
+    return (
+        supabase
+        .table("desafios")
+        .delete()
+        .eq("id", id_desafio)
         .execute()
+    )
